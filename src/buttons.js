437a6869939df42e31c64c5cf30a700106346762
Stanislau <xmix25@gmail.com>
2023-04-29 23:21:20 +0300

    feat: add button render function

--- src/buttons.js
+++ src/buttons.js
@@ -2,257 +2,321 @@ export const btns = [
   {
     name: 'Backquote',
     value: '`',
+    row: 1,
   },
   {
     name: 'Digit1',
     value: '1',
+    row: 1,
   },
   {
     name: 'Digit2',
     value: '2',
+    row: 1,
   },
   {
     name: 'Digit3',
     value: '3',
+    row: 1,
   },
   {
     name: 'Digit4',
     value: '4',
+    row: 1,
   },
   {
     name: 'Digit5',
     value: '5',
+    row: 1,
   },
   {
     name: 'Digit6',
     value: '6',
+    row: 1,
   },
   {
     name: 'Digit7',
     value: '7',
+    row: 1,
   },
   {
     name: 'Digit8',
     value: '8',
+    row: 1,
   },
   {
     name: 'Digit9',
     value: '9',
+    row: 1,
   },
   {
     name: 'Digit0',
     value: '0',
+    row: 1,
   },
   {
     name: 'Minus',
     value: '-',
+    row: 1,
   },
   {
     name: 'Equal',
     value: '=',
+    row: 1,
   },
   {
     name: 'Backspace',
     value: 'Backspace',
+    row: 1,
   },
   {
     name: 'Tab',
     value: 'Tab',
+    row: 2,
   },
   {
     name: 'KeyQ',
     value: 'q',
+    row: 2,
   },
   {
     name: 'KeyW',
     value: 'w',
+    row: 2,
   },
   {
     name: 'KeyE',
     value: 'e',
+    row: 2,
   },
   {
     name: 'KeyR',
     value: 'r',
+    row: 2,
   },
   {
     name: 'KeyT',
     value: 't',
+    row: 2,
   },
   {
     name: 'KeyY',
     value: 'y',
+    row: 2,
   },
   {
     name: 'KeyU',
     value: 'u',
+    row: 2,
   },
   {
     name: 'KeyI',
     value: 'i',
+    row: 2,
   },
   {
     name: 'KeyO',
     value: 'o',
+    row: 2,
   },
   {
     name: 'KeyP',
     value: 'p',
+    row: 2,
   },
   {
     name: 'BracketLeft',
     value: '[',
+    row: 2,
   },
   {
     name: 'BracketRight',
     value: ']',
+    row: 2,
   },
   {
     name: 'Backslash',
     value: '\\',
+    row: 2,
   },
   {
     name: 'Delete',
     value: 'Delete',
+    row: 2,
   },
   {
     name: 'CapsLock',
     value: 'CapsLock',
+    row: 3,
   },
   {
     name: 'KeyA',
     value: 'a',
+    row: 3,
   },
   {
     name: 'KeyS',
     value: 's',
+    row: 3,
   },
   {
     name: 'KeyD',
     value: 'd',
+    row: 3,
   },
   {
     name: 'KeyF',
     value: 'f',
+    row: 3,
   },
   {
     name: 'KeyG',
     value: 'g',
+    row: 3,
   },
   {
     name: 'KeyH',
     value: 'h',
+    row: 3,
   },
   {
     name: 'KeyJ',
     value: 'j',
+    row: 3,
   },
   {
     name: 'KeyK',
     value: 'k',
+    row: 3,
   },
   {
     name: 'KeyL',
     value: 'l',
+    row: 3,
   },
   {
     name: 'Semicolon',
     value: ';',
+    row: 3,
   },
   {
     name: 'Quote',
     value: "'",
+    row: 3,
   },
   {
     name: 'Enter',
     value: 'Enter',
+    row: 3,
   },
   {
     name: 'ShiftLeft',
     value: 'Shift',
+    row: 4,
   },
   {
     name: 'KeyZ',
     value: 'z',
+    row: 4,
   },
   {
     name: 'KeyX',
     value: 'x',
+    row: 4,
   },
   {
     name: 'KeyC',
     value: 'c',
+    row: 4,
   },
   {
     name: 'KeyV',
     value: 'v',
+    row: 4,
   },
   {
     name: 'KeyB',
     value: 'b',
+    row: 4,
   },
   {
     name: 'KeyN',
     value: 'n',
+    row: 4,
   },
   {
     name: 'KeyM',
     value: 'm',
+    row: 4,
   },
   {
     name: 'Comma',
     value: ',',
+    row: 4,
   },
   {
     name: 'Period',
     value: '.',
+    row: 4,
   },
   {
     name: 'Slash',
     value: '/',
+    row: 4,
   },
   {
     name: 'ArrowUp',
     value: 'ArrowUp',
+    row: 4,
   },
   {
     name: 'ShiftRight',
     value: 'Shift',
+    row: 4,
   },
   {
     name: 'ControlLeft',
     value: 'Control',
+    row: 5,
   },
   {
     name: 'MetaLeft',
     value: 'Meta',
+    row: 5,
   },
   {
     name: 'AltLeft',
     value: 'Alt',
+    row: 5,
   },
   {
     name: 'Space',
     value: ' ',
+    row: 5,
   },
   {
     name: 'AltRight',
     value: 'Alt',
+    row: 5,
   },
   {
     name: 'ArrowLeft',
     value: 'ArrowLeft',
+    row: 5,
   },
   {
     name: 'ArrowDown',
     value: 'ArrowDown',
+    row: 5,
   },
   {
     name: 'ArrowRight',
     value: 'ArrowRight',
+    row: 5,
   },
   {
     name: 'ControlRight',
     value: 'Control',
+    row: 5,
   },
 ];
--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+import { btns } from './buttons.js';
+
 const main = document.querySelector('.main');
+
+function createKeyboard() {
+  const kb = document.createElement('div');
+  for (let i = 1; i <= 5; i++) {
+    const row = document.createElement('div');
+    row.className = `keyboard__row keyboard__row-${i}`;
+    kb.append(row);
+  }
+  kb.className = 'keyboard';
+  main.append(kb);
+  renderButtons();
+}
+function renderButtons() {
+  const rows = document.querySelectorAll('.keyboard__row');
+  rows.forEach((row,i) => {
+    btns.filter(btn => btn.row === i + 1).forEach( btn => {
+      const key = document.createElement('button');
+      key.classList.add('key');
+      key.textContent = btn.value;
+      row.append(key)
+    })
+  })
+}
+
+createKeyboard();
