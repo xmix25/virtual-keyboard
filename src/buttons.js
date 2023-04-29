c40555b334b690672cdb1165250a279c2f41e81f
Stanislau <xmix25@gmail.com>
2023-04-29 19:32:32 +0300

    feat: add html template and buttons.js

---
+++ src/buttons.js
@@ -0,0 +1,258 @@
+export const btns = [
+  {
+    name: 'Backquote',
+    value: '`',
+  },
+  {
+    name: 'Digit1',
+    value: '1',
+  },
+  {
+    name: 'Digit2',
+    value: '2',
+  },
+  {
+    name: 'Digit3',
+    value: '3',
+  },
+  {
+    name: 'Digit4',
+    value: '4',
+  },
+  {
+    name: 'Digit5',
+    value: '5',
+  },
+  {
+    name: 'Digit6',
+    value: '6',
+  },
+  {
+    name: 'Digit7',
+    value: '7',
+  },
+  {
+    name: 'Digit8',
+    value: '8',
+  },
+  {
+    name: 'Digit9',
+    value: '9',
+  },
+  {
+    name: 'Digit0',
+    value: '0',
+  },
+  {
+    name: 'Minus',
+    value: '-',
+  },
+  {
+    name: 'Equal',
+    value: '=',
+  },
+  {
+    name: 'Backspace',
+    value: 'Backspace',
+  },
+  {
+    name: 'Tab',
+    value: 'Tab',
+  },
+  {
+    name: 'KeyQ',
+    value: 'q',
+  },
+  {
+    name: 'KeyW',
+    value: 'w',
+  },
+  {
+    name: 'KeyE',
+    value: 'e',
+  },
+  {
+    name: 'KeyR',
+    value: 'r',
+  },
+  {
+    name: 'KeyT',
+    value: 't',
+  },
+  {
+    name: 'KeyY',
+    value: 'y',
+  },
+  {
+    name: 'KeyU',
+    value: 'u',
+  },
+  {
+    name: 'KeyI',
+    value: 'i',
+  },
+  {
+    name: 'KeyO',
+    value: 'o',
+  },
+  {
+    name: 'KeyP',
+    value: 'p',
+  },
+  {
+    name: 'BracketLeft',
+    value: '[',
+  },
+  {
+    name: 'BracketRight',
+    value: ']',
+  },
+  {
+    name: 'Backslash',
+    value: '\\',
+  },
+  {
+    name: 'Delete',
+    value: 'Delete',
+  },
+  {
+    name: 'CapsLock',
+    value: 'CapsLock',
+  },
+  {
+    name: 'KeyA',
+    value: 'a',
+  },
+  {
+    name: 'KeyS',
+    value: 's',
+  },
+  {
+    name: 'KeyD',
+    value: 'd',
+  },
+  {
+    name: 'KeyF',
+    value: 'f',
+  },
+  {
+    name: 'KeyG',
+    value: 'g',
+  },
+  {
+    name: 'KeyH',
+    value: 'h',
+  },
+  {
+    name: 'KeyJ',
+    value: 'j',
+  },
+  {
+    name: 'KeyK',
+    value: 'k',
+  },
+  {
+    name: 'KeyL',
+    value: 'l',
+  },
+  {
+    name: 'Semicolon',
+    value: ';',
+  },
+  {
+    name: 'Quote',
+    value: "'",
+  },
+  {
+    name: 'Enter',
+    value: 'Enter',
+  },
+  {
+    name: 'ShiftLeft',
+    value: 'Shift',
+  },
+  {
+    name: 'KeyZ',
+    value: 'z',
+  },
+  {
+    name: 'KeyX',
+    value: 'x',
+  },
+  {
+    name: 'KeyC',
+    value: 'c',
+  },
+  {
+    name: 'KeyV',
+    value: 'v',
+  },
+  {
+    name: 'KeyB',
+    value: 'b',
+  },
+  {
+    name: 'KeyN',
+    value: 'n',
+  },
+  {
+    name: 'KeyM',
+    value: 'm',
+  },
+  {
+    name: 'Comma',
+    value: ',',
+  },
+  {
+    name: 'Period',
+    value: '.',
+  },
+  {
+    name: 'Slash',
+    value: '/',
+  },
+  {
+    name: 'ArrowUp',
+    value: 'ArrowUp',
+  },
+  {
+    name: 'ShiftRight',
+    value: 'Shift',
+  },
+  {
+    name: 'ControlLeft',
+    value: 'Control',
+  },
+  {
+    name: 'MetaLeft',
+    value: 'Meta',
+  },
+  {
+    name: 'AltLeft',
+    value: 'Alt',
+  },
+  {
+    name: 'Space',
+    value: ' ',
+  },
+  {
+    name: 'AltRight',
+    value: 'Alt',
+  },
+  {
+    name: 'ArrowLeft',
+    value: 'ArrowLeft',
+  },
+  {
+    name: 'ArrowDown',
+    value: 'ArrowDown',
+  },
+  {
+    name: 'ArrowRight',
+    value: 'ArrowRight',
+  },
+  {
+    name: 'ControlRight',
+    value: 'Control',
+  },
+];
